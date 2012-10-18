ccde7f18a10d514cc9e7cfbbba2b0dcc13b23c3f
abani.abani@gmail.com <abani.abani@gmail.com@4258fca3-fc49-5ee0-a4a2-736f2ca329f5>
2012-10-18 02:07:34 +0000

    A client bundle for OSGi Demo

---
+++ trunk/osgidemos/client/src/main/java/org/client/activator/ClientActivator.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.client.activator;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.demo.services.GreetService;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+/**
+ * @author abani
+ *
+ */
+public class ClientActivator implements BundleActivator {
+	private static final Logger LOGGER = Logger.getLogger( ClientActivator.class.getName() );
+	public void start(BundleContext ctx) throws Exception {
+		ServiceReference ser = ctx.getServiceReference(GreetService.class.getName());
+		GreetService greetService = (GreetService) ctx.getService( ser ) ;
+		LOGGER.log(Level.INFO, "Message is :: {0} ", greetService.greetUser("abani"));
+	}
+
+	public void stop(BundleContext arg0) throws Exception {
+		
+	}
+
+}
